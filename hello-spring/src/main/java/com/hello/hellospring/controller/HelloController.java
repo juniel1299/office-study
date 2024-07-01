eb310d1cf79fa36232546504688fc4b8c52f726e
juniel1299 <youngs5440@gmail.com>
2024-07-01 12:11:16 +0900

    0701-spring 기초
    [upload] 스프링 기초부터 다시

---
+++ hello-spring/src/main/java/com/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    //
+    @GetMapping("hello-string")
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name;
+    }
+}
+
